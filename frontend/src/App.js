bf9048d804179eeda696f573211c5df76c3af8fc
hkwon <mr.hyuk.kwon@gmail.com>
2020-02-16 15:49:20 +0900

    feat(react): main page layout prototype - yarn add styled-componets - yarn add material-ui/core - yarn add material-ui/icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,196 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import {
+  Typography,
+  Card,
+  CardActions,
+  CardContent,
+  Button
+} from "@material-ui/core";
+import {
+  RestaurantOutlined,
+  HotelOutlined,
+  LocalSeeOutlined,
+  EmojiEmotionsOutlined
+} from "@material-ui/icons";
+
+import "./App.css";
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  // margin: ${props => (props.margin ? "1rem" : "0")};
+  margin: 0.5rem;
+  .card {
+    margin: 0.3rem;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FlexRow margin>
+        <Typography variant="h3" component="h1">
+          Eat
+        </Typography>
+        <Typography variant="h3" component="h1">
+          Play
+        </Typography>
+        <Typography variant="h3" component="h1">
+          See
+        </Typography>
+        <Typography variant="h3" component="h1">
+          Stay
+        </Typography>
+      </FlexRow>
+      <Typography variant="subtitle1" align="center">
+        지역을 선택해주세요.
+      </Typography>
+      <FlexRow>
+        <Card className="card">
+          <CardContent>
+            <Typography variant="h6">London</Typography>
+            <Typography variant="subtitle2" gutterBottom>
+              UK
+            </Typography>
+            <FlexRow>
+              <RestaurantOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <EmojiEmotionsOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <LocalSeeOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <HotelOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+        <Card className="card">
+          <CardContent>
+            <Typography variant="h6">London</Typography>
+            <Typography variant="subtitle2" gutterBottom>
+              UK
+            </Typography>
+            <FlexRow>
+              <RestaurantOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <EmojiEmotionsOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <LocalSeeOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <HotelOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+        <Card className="card">
+          <CardContent>
+            <Typography variant="h6">London</Typography>
+            <Typography variant="subtitle2" gutterBottom>
+              UK
+            </Typography>
+            <FlexRow>
+              <RestaurantOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <EmojiEmotionsOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <LocalSeeOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <HotelOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+        <Card className="card">
+          <CardContent>
+            <Typography variant="h6">London</Typography>
+            <Typography variant="subtitle2" gutterBottom>
+              UK
+            </Typography>
+            <FlexRow>
+              <RestaurantOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <EmojiEmotionsOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <LocalSeeOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+            <FlexRow>
+              <HotelOutlined />
+              <Typography variant="subtitle2" component="span">
+                0개
+              </Typography>
+            </FlexRow>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </FlexRow>
+    </>
   );
 }
 
